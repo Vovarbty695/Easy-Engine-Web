23d81cb17ab2469f6fcfbbe013ef38efe19c0ced
Vovarbty <vovarbty288@gmail.com>
2025-04-20 12:41:59 +0300

    Documentation and TS

---
+++ engine.ts
@@ -0,0 +1,19 @@
+function print(text: string){
+    console.log(text);
+}
+
+function println(text: string){
+    console.log(text + "\n");
+}
+
+function clear(){
+    console.clear();
+}
+
+function dprint(text: string){
+    document.write(text);
+}
+
+function dprintln(text: string){
+    document.writeln(text);
+}
